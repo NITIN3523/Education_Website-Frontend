9d2bb625ec4edf6877553c3d4bf64cf8d6f77a64
NITIN3523 <vp35233523@gmail.com>
2022-07-06 23:57:03 +0530

    changes

--- src/Component/About/Review.js
+++ src/Component/About/Review.js
@@ -17,7 +17,7 @@ export default function Review() {
         <div className='mx-3 my-20'>
             <h1 className='text-center text-white text-4xl font-bold'>Students Review</h1>
             <div className='flex gap-8 justify-around mt-10'>
-                <div className='text-white flex items-center'><i className="fa-solid fa-chevron-left fa-4x"
+                <div className='text-white flex items-center'><i className="fa-solid fa-chevron-left fa-4x hover:bg-gray-300 p-3 rounded text-white hover:text-slate-900"
                     onClick={() => {
                         if (id == 0) {
                             setimgreview(false)
@@ -31,17 +31,18 @@ export default function Review() {
                         {imgreview ? Sturev[id].desc : Sturev[0].desc}
                     </p>
                     <div className='flex flex-col items-center mt-3'>
-                        <img src={imgreview ? Sturev[id].src : Sturev[0].src} alt="Students" className='h-20 w-20 rounded-full outline outline-purple-700 outline-[0.35rem] mt-5' />
+                        <img src={imgreview ? Sturev[id].src : Sturev[0].src} alt="Students" className='h-20 w-20 rounded-full outline outline-slate-600 outline-[0.5rem] mt-5' />
                         <h1 className='text-xl font-medium text-slate-400 mt-2 text-center'>{imgreview ? Sturev[id].name : Sturev[0].name}</h1>
                     </div>
                 </div>
-                <div className='text-white flex items-center'><i className="fa-solid fa-chevron-right fa-4x"
+                <div className='text-white flex items-center'><i className="fa-solid fa-chevron-right fa-4x hover:bg-gray-300 p-3 rounded text-white hover:text-slate-900"
                     onClick={() => {
                         if (id == (Sturev.length - 1)) {
                             setimgreview(false)
                         }
                         setid(id + 1)
-                    }}></i></div>
+                    }}>
+                </i></div>
             </div>
         </div>
     )
